b39a9f76ccce520a57a8793c2917d3230abd63df
nossbigg <chenggibson@gmail.com>
2022-03-02 18:10:26 +0800

    [max-jsx-lines] Add example

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "@nossbigg/eslint-plugin-clean-code-react"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@nossbigg/clean-code-react/max-jsx-lines": [2, { maxLines: 2 }],
+  },
 };
---
+++ src/max-jsx-lines.tsx
@@ -0,0 +1,5 @@
+const MyComponent = () => (
+  <div>
+    <div></div>
+  </div>
+);
