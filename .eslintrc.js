03ff14d6912670d2faf76f80b7c9fd1af01a1bfe
nossbigg <chenggibson@gmail.com>
2022-03-02 22:18:06 +0800

    [max-jsx-lines] Adjust example

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: ["@typescript-eslint", "@nossbigg/eslint-plugin-clean-code-react"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@nossbigg/clean-code-react/max-jsx-lines": [2, { maxLines: 2 }],
+    "@nossbigg/clean-code-react/max-jsx-lines": [2],
     "@nossbigg/clean-code-react/max-jsx-expression-lines": [2],
   },
 };
