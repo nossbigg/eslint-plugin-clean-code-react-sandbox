2304dc743d35ba14932f77353f4fe7ab5cc2079f
nossbigg <chenggibson@gmail.com>
2022-03-07 01:26:37 +0800

    [no-typedefs-in-large-component-file] Add example

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     "@nossbigg/clean-code-react/no-other-component-in-large-component-file": [
       2,
     ],
+    "@nossbigg/clean-code-react/no-typedefs-in-large-component-file": [2],
   },
 };
---
+++ src/no-typedefs-in-large-component-file.tsx
@@ -0,0 +1,60 @@
+type MyLargeComponentProps = { name: string };
+
+type SomeOtherType = {};
+
+const MyLargeComponent: React.FC<MyLargeComponentProps> = () => {
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+  1 == 1;
+
+  return (
+    <div>
+      1
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      2
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+};
